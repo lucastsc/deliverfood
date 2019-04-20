f3a74757716ccc7d6950b93f87e83452fa187868
lucas <lucastsc@gmail.com>
2019-04-19 22:50:12 -0300

    bugs corrected, app compiling and firebase working

--- deliverfood/lib/HomePage.dart
+++ deliverfood/lib/HomePage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,12 +7,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Deliverfood"),
         backgroundColor: Colors.lightBlue,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              color: Colors.blue,
+              child: Text("Firebase"),
+              onPressed: (){
+                Firestore.instance.collection("teste2").document("teste2").setData({"teste1":"teste2"});
+              },
+            )
+          ],
+        ),
       ),
     );
   }
--- deliverfood/lib/main.dart
+++ deliverfood/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:deliverfood/HomePage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main(){
+
   runApp(MaterialApp(
     home: HomePage(),
     debugShowCheckedModeBanner: false,
